76baff6c58517d4e82d0b93f9f33d97e056dab2f
aannaasttasia <nastyahavruliyk@gmail.com>
2023-03-29 11:18:19 +0300

    search-bar

--- src/components/Movies/Movies.tsx
+++ src/components/Movies/Movies.tsx
@@ -3,10 +3,14 @@ import { Movie } from '../../shared/interfaces';
 // @ts-ignore
 import { ApiRequest } from '../ApiRequest/ApiRequest.tsx';
 
-export default class Movies extends React.Component<any,{ movies: Movie[]}>{
-    constructor(props: Movie[]){
+interface MoviesComponentType{
+  movies: Movie[],
+  term: string
+}
+export default class Movies extends React.Component<any,MoviesComponentType>{
+    constructor(props: MoviesComponentType){
         super(props);
-        this.state = {movies: []};
+        this.state = {movies: [], term: ''};
     }
 
     async componentDidMount() {
@@ -15,22 +19,44 @@ export default class Movies extends React.Component<any,{ movies: Movie[]}>{
         console.log(moviesList)
     }
 
-    mappedInfo = (movies: Movie[]) => {
+    onInputChange = (event:any) =>{
+      this.setState({term: event.target.value})
+  }
+
+    mappedInfo = (movies: Movie[], term: string) => {
         return (
           <div >
-            {movies.map((movie: Movie, id:number) => {
+            <div className="search-bar">
+                <input 
+                    type="text"
+                    placeholder='Search for a movie'
+                    value={this.state.term}
+                    onChange={this.onInputChange}
+                />
+            </div>
+            <div className="">
+            {movies.filter(movie => {
+              if (term === '') {
+                return movie;
+              } else if (movie.title.toLowerCase().includes(term.toLowerCase())) {
+                return movie;
+              }
+            }).map((movie: Movie, id: number) => {
               return(
-                <div className="movie">
-                    <h1>{movie.title}</h1>
-                    <h1>{movie.overview}</h1>
+                <div className="movie" key={id}>
+                    <h1 className='movie-title'>{movie.title}</h1>
+                    <p className='movie-overview'>{movie.overview}</p>
+                    <p className='movie-release_date'>{movie.release_date}</p>
+                    <p className='movie-vote_average'>{movie.vote_average}</p>
                 </div>
               )
             })}
+            </div>
           </div>
         );
       };
 
     render(){
-        return this.mappedInfo(this.state.movies)
+        return this.mappedInfo(this.state.movies, this.state.term)
     }
 }
